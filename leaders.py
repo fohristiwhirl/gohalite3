c8c6896e9c0300edf4a843beeefac981bddaa9dd
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2018-10-05 22:57:30 +0100

    fmt

--- leaders.py
+++ leaders.py
@@ -4,8 +4,6 @@ data = requests.get("https://api.2018.halite.io/v1/api/leaderboard").json()
 
 for item in data:
 
-
-
 	print("{0:>3}  {1:>18}  {2:<6}  {3:.2f}  v{4}".format(
 		item["rank"],
 		item["username"],
