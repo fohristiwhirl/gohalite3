5ac366c586c77ae8f4edd43faa04b55dde42c473
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2018-10-20 01:16:22 +0100

    56

--- match4.bat
+++ match4.bat
@@ -1,1 +1,1 @@
-.\halite.exe bot.exe bot.exe old\v9.exe old\v9.exe --height 64 --width 64 --no-logs -i replays_local --results-as-json
+.\halite.exe bot.exe bot.exe old\v9.exe old\v9.exe --height 56 --width 56 --no-logs -i replays_local --results-as-json
