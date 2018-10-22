3b76a115125216393a45a9da6ac84524e6a4f28a
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2018-10-22 23:13:35 +0100

    Update halite.py

--- halite.py
+++ halite.py
@@ -3,7 +3,7 @@ REPLAY_FOLDER = "replays_local"
 
 # ------------------------------------------------------------------
 
-import json, subprocess, sys
+import json, subprocess, sys, time
 
 add = ["--no-compression", "--no-logs", "--results-as-json", "-i", REPLAY_FOLDER]
 
@@ -15,7 +15,9 @@ else:
 new_args_string = " ".join(new_args)
 cmd = "__halite.exe {}".format(new_args_string)
 
+start_time = time.time()
 output = subprocess.check_output(cmd).decode("ascii")
+elapsed_time = time.time() - start_time
 
 j = json.loads(output)
 
@@ -24,5 +26,7 @@ useful = dict()
 for key in ["map_width", "map_height", "replay", "stats", "map_seed"]:
 	if key in j:
 		useful[key] = j[key]
+
+useful["time"] = time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
 
 print(json.dumps(useful, indent = 4))
