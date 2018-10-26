16a83afeb7cd241676cdfb30df3b7f665b3ac0b8
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2018-10-26 23:20:58 +0100

    stuff for iodine

--- halite.py
+++ halite.py
@@ -1,29 +1,28 @@
-
 REPLAY_FOLDER = "replays_local"
-
-EXE = "C:\\Users\\Owner\\github\\dubnium\\dubnium.exe"
+IODINE_DIR = "C:\\Users\\Owner\\github\\iodine"
 
 # ------------------------------------------------------------------
 
 import json, subprocess, sys, time
 
-add = ["--no-compression", "--no-logs", "--results-as-json", "-i", REPLAY_FOLDER]
+initial_args = [IODINE_DIR, "-i", REPLAY_FOLDER]
 
 if sys.argv[0] == "python":
-	new_args = sys.argv[2:] + add
+	new_args = initial_args + sys.argv[2:]
 else:
-	new_args = sys.argv[1:] + add
+	new_args = initial_args + sys.argv[1:]
 
 for n, arg in enumerate(new_args):
 	new_args[n] = '"' + arg + '"'
 
 new_args_string = " ".join(new_args)
-cmd = "{} {}".format(EXE, new_args_string)
+cmd = "{} {}".format("electron", new_args_string)
 
 start_time = time.time()
-output = subprocess.check_output(cmd).decode("ascii")
+output = subprocess.check_output(cmd, shell=True).decode("ascii")
 elapsed_time = time.time() - start_time
 
+"""
 j = json.loads(output)
 
 useful = dict()
@@ -35,3 +34,4 @@ for key in ["map_width", "map_height", "replay", "stats", "map_seed"]:
 useful["time"] = time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
 
 print(json.dumps(useful, indent = 4))
+"""
