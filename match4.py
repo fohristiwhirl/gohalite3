29c3df62499c89598094cf7bb8fb4f8a875cf9f1
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2018-10-27 21:50:15 +0100

    working match4 script

--- match4.py
+++ match4.py
@@ -1,40 +1,30 @@
 import json, random, subprocess
 
-processes = [
+REPLAY_FOLDER = "replays_local"
+
+bots = [
 	"bot.exe",
-	".\\otherbots\\v105\\mybot.exe",
-	".\\otherbots\\v105\\mybot.exe",
-	".\\otherbots\\v105\\mybot.exe",
+	"bot.exe",
+	".\\old\\v6.exe",
+	"python dnb.py",
 ]
 
-FORCED_WIDTH = None
-FORCED_HEIGHT = None
-
 # ------------------------------------------------------------------------
-
-map_sizes = [80, 80, 88, 88, 96, 96, 96, 104, 104, 104, 104, 112, 112, 112, 120, 120, 128, 128]
 
 scores = [0,0,0,0]
 positions = [0,1,2,3]
 
-print("{} --- {} --- {} --- {}".format(processes[0], processes[1], processes[2], processes[3]))
+print("{} --- {} --- {} --- {}".format(bots[0], bots[1], bots[2], bots[3]))
 
 while 1:
 
 	random.shuffle(positions)
 
-	if (not FORCED_WIDTH) or (not FORCED_HEIGHT):
-		base_size = random.choice(map_sizes)
-		width = base_size * 3
-		height = base_size * 2
-	else:
-		width = FORCED_WIDTH
-		height = FORCED_HEIGHT
+	tmp_positions = [bots[positions[0]], bots[positions[1]], bots[positions[2]], bots[positions[3]]]
 
-	output = subprocess.check_output(
-		"halite.exe -d \"{} {}\" --no-compression -q \"{}\" \"{}\" \"{}\" \"{}\"".format(width, height,
-			processes[positions[0]], processes[positions[1]], processes[positions[2]], processes[positions[3]]
-			)).decode("ascii")
+	args = ["__halite.exe", "-i", REPLAY_FOLDER, "--results-as-json", "--no-logs"] + tmp_positions
+
+	output = subprocess.check_output(args).decode("ascii")
 
 	result = json.loads(output)
 
