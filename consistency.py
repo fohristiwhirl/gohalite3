206c6825afd597f0bbf770dc23fa67bd43dc5d80
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2018-10-28 23:01:49 +0000

    engine consistency checker

---
+++ consistency.py
@@ -0,0 +1,21 @@
+one = "__halite.exe"
+two = "C:\\Users\\Owner\\github\\dubnium\\dubnium.exe"
+
+bot = "bot.exe"
+
+import json, random, subprocess, sys
+
+random.seed()
+game_seed = random.randint(0, 2000000000)
+
+args = [bot for n in range(4)] + ["--no-logs", "--no-replay", "--results-as-json", "-s", str(game_seed)]
+
+result_one = subprocess.check_output([one] + args, shell=True)
+result_two = subprocess.check_output([two] + args, shell=True)
+
+j_one = json.loads(result_one)
+j_two = json.loads(result_two)
+
+print("Seed {}".format(game_seed))
+for key in j_one["stats"]:
+	print("     pass" if j_one["stats"][key]["score"] == j_two["stats"][key]["score"] else "     FAIL <-----------------------------------")
