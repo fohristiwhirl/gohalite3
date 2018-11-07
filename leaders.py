3f100cfffd5887966841e4b61a6dcd19b5ba9a85
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2018-11-07 17:30:43 +0000

    only dl what we need

--- leaders.py
+++ leaders.py
@@ -1,12 +1,14 @@
 import requests
 
-data = requests.get("https://api.2018.halite.io/v1/api/leaderboard").json()
+COUNT = 38
+
+data = requests.get("https://api.2018.halite.io/v1/api/leaderboard?limit={}".format(COUNT)).json()
 
 data.sort(key = lambda foo : foo["mu"], reverse = True)
 
 print()
 
-for n, item in enumerate(data[:38]):
+for n, item in enumerate(data):
 
 	print(" {0:>3} {1:<6}  {2:>22} v{3:<4}  {4:.2f} +/- {5:.2f}    {6}".format(
 		n + 1,
